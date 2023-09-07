7ded2c6fb9541bdc753459f3f06525d412c86777
yurii2007 <135147654+yurii2007@users.noreply.github.com>
2023-09-07 21:00:57 +0000

    Deploying to gh-pages from @ yurii2007/goit-react-hw-08-phonebook@bd4af0e748c0d13c957ccb5d5fdb247ca73c0105 🚀

--- static/js/415.def96ecd.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{5415:function(e,t,r){r.r(t);var a=r(3329);t.default=function(){return(0,a.jsx)("div",{children:"Home"})}}}]);
-//# sourceMappingURL=415.def96ecd.chunk.js.map
---
+++ static/js/592.8a3fb188.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[592],{3629:function(t,e,a){a.r(e),a.d(e,{default:function(){return o}});var r=a.p+"static/media/prykol.969eedb5dba82da0c028.png",n=a(3329),o=function(){return(0,n.jsx)("main",{children:(0,n.jsx)("img",{style:{position:"absolute",top:"50%",left:"50%",transform:"translate(-50%,-50%)",width:300,height:300},src:r,alt:"nema prykola"})})}}}]);
+//# sourceMappingURL=592.8a3fb188.chunk.js.map
